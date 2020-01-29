a084adaa25f91aef8c2bf661cb8c6d7ef86e511a
rsp9u <gctwt852@yahoo.co.jp>
2020-01-30 00:50:28 +0900

    Customize menu min width

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,3 +1,14 @@
 <template lang="pug">
   router-view
 </template>
+
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+$dropdown-menu-min-width: 3em;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+</style>
--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
 
 Vue.config.productionTip = false;
 Vue.use(Buefy);
