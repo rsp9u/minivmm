ec0f21d286cd3c2c96c2de03d2af682f6b8b9a82
rsp9u <gctwt852@yahoo.co.jp>
2021-01-30 22:07:08 +0900

    Add used resoruce infoes to system resource metrics

--- metrics/resource/disk_linux.go
+++ metrics/resource/disk_linux.go
@@ -2,9 +2,16 @@ package resource
 
 import "golang.org/x/sys/unix"
 
-func getDiskSize(path string) uint64 {
+func getDiskSizeTotal(path string) uint64 {
 	var stat unix.Statfs_t
 	unix.Statfs(path, &stat)
 
 	return stat.Blocks * uint64(stat.Bsize)
+}
+
+func getDiskSizeUsed(path string) uint64 {
+	var stat unix.Statfs_t
+	unix.Statfs(path, &stat)
+
+	return (stat.Blocks - stat.Bavail) * uint64(stat.Bsize)
 }
--- metrics/resource/sys.go
+++ metrics/resource/sys.go
@@ -8,9 +8,11 @@ import (
 
 // SysMetric is the metrics of system resrouces.
 type SysMetric struct {
-	CPUCores    int    `json:"minivmm_sys_cpu_cores"`
-	MemoryBytes uint64 `json:"minivmm_sys_memory_bytes"`
-	DiskBytes   uint64 `json:"minivmm_sys_disk_bytes"`
+	CPUCores        int    `json:"minivmm_sys_cpu_cores"`
+	MemoryBytes     uint64 `json:"minivmm_sys_memory_bytes"`
+	MemoryBytesUsed uint64 `json:"minivmm_sys_memory_bytes_used"`
+	DiskBytes       uint64 `json:"minivmm_sys_disk_bytes"`
+	DiskBytesUsed   uint64 `json:"minivmm_sys_disk_bytes_used"`
 }
 
 // GetSysMetric returns the system resource metrics.
@@ -28,8 +30,10 @@ func GetSysMetric() (*SysMetric, error) {
 		return nil, err
 	}
 	m.MemoryBytes = memory.Total
+	m.MemoryBytesUsed = memory.Used
 
-	m.DiskBytes = getDiskSize(minivmm.C.Dir)
+	m.DiskBytes = getDiskSizeTotal(minivmm.C.Dir)
+	m.DiskBytesUsed = getDiskSizeUsed(minivmm.C.Dir)
 
 	return &m, nil
 }
